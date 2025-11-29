0e009d9fae6280217bd379e9339ff30da6ef17e9
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-29 10:59:24 +0800

    Update readme.md and add live url

--- Documents/appdev1-final-exam/vite.config.js
+++ Documents/appdev1-final-exam/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+base: "/YOUR_REPOSITORY_NAME/",
+plugins: [react()],
+});
+
