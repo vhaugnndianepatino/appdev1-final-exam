42e6ddc76926478320b25db38ef09235aa4505fa
Vhaugnn Diane Patiño <vhaugnndianepatino@gmail.com>
2025-11-29 09:25:42 +0800

    Implement Home component

---
+++ Documents/appdev1-final-exam/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Home() {
+return (
+<main style={{ padding: 20 }}>
+<h1>Welcome to Todo App — AppDev1 Final</h1>
+<p>Use the navigation to go to Login or Todos.</p>
+</main>
+)
+}g
